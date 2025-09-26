b4f9f8904b0c322a52a9e47fea192785a7607a98
TatiRoque <tatiana@roque.com.ar>
2025-09-26 20:19:52 -0300

    chore: initialize project structure

---
+++ DeliveryGo/DeliveryGo/Core/Services/Implementations/ShippingService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeliveryGo.Core.Services.Implementations
+{
+    public class ShippingService
+    {
+    }
+}
---
+++ DeliveryGo/DeliveryGo/Core/Shared/Contracts/IShippingService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeliveryGo.Core.Shared.Contracts
+{
+    public interface IShippingService
+    {
+    }
+}
